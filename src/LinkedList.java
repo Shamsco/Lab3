c6dc64c209bfa366ca50fae610a97ffd338a8497
shamissa <shamissa@10.0.2.15>
2016-10-18 16:19:22 -0600

    Added Add Element

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -3,7 +3,9 @@ public class LinkedList {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ListElement le = new ListElement();
+		le.setData(5);
+		
 	}
 
 }
--- src/ListElement.java
+++ src/ListElement.java
@@ -1,18 +1,34 @@
 
 public class ListElement {
 	private ListElement next ;
-	private ListElement previous;
+	private ListElement head;
 	private int data;
 	
 	public ListElement(){
 		this.data = 0;
 		this.next = null;
-		this.previous = null;
+		this.head = null;
 	}
 	public void setData(int Data){
-		this.data = data;
+		this.data = Data;
 	}
 	public int getData(){
 		return this.data;
+	}
+	public void addElement(ListElement le){
+		if(this.head == null){
+			ListElement newElement = new ListElement();
+			newElement.setData(le.getData());
+			this.head = newElement;
+		}
+		else{
+			ListElement curr = this.head;
+			while(curr != null){
+				curr = this.next;
+			}
+			ListElement newElement = new ListElement();
+			newElement.setData(le.getData());
+			curr = newElement;
+		}
 	}
 }
