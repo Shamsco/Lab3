cfd3c684cb6d072ce91ae4b91b347acc8e7e6557
shamissa <shamissa@10.0.2.15>
2016-10-18 15:57:08 -0600

    First Commit, Nothing major.

---
+++ src/ListElement.java
@@ -0,0 +1,18 @@
+
+public class ListElement {
+	private ListElement next ;
+	private ListElement previous;
+	private int data;
+	
+	public ListElement(){
+		this.data = 0;
+		this.next = null;
+		this.previous = null;
+	}
+	public void setData(int Data){
+		this.data = data;
+	}
+	public int getData(){
+		return this.data;
+	}
+}
